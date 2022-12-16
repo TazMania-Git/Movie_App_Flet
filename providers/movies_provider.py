a11ba0bc4c1779348674c8cc95d74336132c48f4
TazMania-Git <nallinon@gmail.com>
2022-12-16 02:36:35 -0300

    furia

--- providers/movies_provider.py
+++ providers/movies_provider.py
@@ -1,19 +1,14 @@
 import requests
 import json
 import sys
+
 ### FUNCIONA #####
-# sys.path.append(r'E:\Mis Documentos\Flet Flutter apps\Movie_App_Flet\models')
-# import now_playing_response
-##################
-
-import models
-# import models.now_playing_response as now
-# from models import now_playing_response
-
-
+sys.path.append(r'F:\Flet Flutter Apps\App\Movie_App_Flet\models')
+import now_playing_response as np
+##################r
 
 response_API = requests.get(url='https://api.themoviedb.org/3/movie/now_playing?api_key=19c0cd86b94cabbe32dfa1440e41e8f9&language=es-ES&page=1')
 data = response_API.text
-result = now_playing_response .Moviesfromdict(json.loads(data))
+result = np.Moviesfromdict(json.loads(data))
 result.results[0].title
-models.now_playing_response
+# models.now_playing_response
